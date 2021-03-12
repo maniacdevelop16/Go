5972428736a8e6d50e3b3486d51c19eaa2104dcc
maniacdevelop16 <43027099+maniacdevelop16@users.noreply.github.com>
2021-03-12 01:03:37 -0800

    Add files via upload

---
+++ cryptography/cryp.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/md5"
+	"crypto/rand"
+	"encoding/hex"
+	"io"
+	"fmt"
+
+)
+
+func createHash(key string) string {
+	hasher := md5.New()
+	hasher.Write([]byte(key))
+	return hex.EncodeToString(hasher.Sum(nil))
+}
+
+func encrypt(data []byte, passphrase string) []byte {
+	block, _ := aes.NewCipher([]byte(createHash(passphrase)))
+	gcm, _ := cipher.NewGCM(block)
+	nonce := make([]byte, gcm.NonceSize())
+	io.ReadFull(rand.Reader,nonce)
+	ciphertext := gcm.Seal(nonce, nonce, data, nil)
+	return ciphertext
+}
+
+
+func decrypt(data []byte, passphrase string) []byte {
+	key := []byte(createHash(passphrase))
+	block, _ := aes.NewCipher(key)
+	gcm, _ := cipher.NewGCM(block)
+	nonceSize := gcm.NonceSize()
+	nonce, ciphertext := data[:nonceSize], data[nonceSize:]
+	plaintext, _ := gcm.Open(nil, nonce, ciphertext, nil)
+	return plaintext
+}
+
+
+
+func main() {
+
+
+	ciphertext := encrypt([]byte("Mayank Srivastava"), "qwerty")
+	fmt.Printf("Encrypted: %x\n", string(ciphertext))
+	plaintext := decrypt(ciphertext, "qwerty")
+	fmt.Printf("Decrypted: %s\n", plaintext)
+
+
+}
